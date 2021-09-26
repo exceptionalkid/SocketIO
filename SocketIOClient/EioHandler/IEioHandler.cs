7cf54681430c2fd6358eefcc36379fc73a4a2b24
Thoufeer <32760382+exceptionalkid@users.noreply.github.com>
2021-09-26 11:56:37 +0400

    Add files via upload
    Initial Commit

---
+++ SocketIOClient/EioHandler/IEioHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace SocketIOClient.EioHandler
+{
+    interface IEioHandler
+    {
+        Task IOConnectAsync(SocketIO io);
+        void Unpack(SocketIO io, string text);
+    }
+}
